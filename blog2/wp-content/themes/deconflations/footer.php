d84b2a8e1b0dbb86484eb6d12ade381535bdf77f
Duncan Kimpton <dgkimpton@users.noreply.github.com>
2022-01-24 07:54:18 +0100

    add a tag cloud

--- blog2/wp-content/themes/deconflations/footer.php
+++ blog2/wp-content/themes/deconflations/footer.php
@@ -1,5 +1,14 @@
 </main>
-<footer class="tail"><small>Design and Content &copy; Copyright Duncan Kimpton 2022</small></footer>
+
+<footer class="tail">
+	<div class="tag-cloud">
+		<h2 class="cloud-head">
+			Common topics
+		</h2>
+		<?php wp_tag_cloud(["smallest" => 1.2, "largest" => 3, "unit" => "em"]); ?>
+	</div>
+	<small>Design and Content &copy; Copyright Duncan Kimpton 2022</small>
+</footer>
 </body>
 
 </html>
